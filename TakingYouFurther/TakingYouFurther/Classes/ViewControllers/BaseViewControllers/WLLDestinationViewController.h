97e32dc3c620db16b96d2c04ebc5d023c0371034
CapJackQ <bichao_li@yahoo.com>
2016-03-01 11:37:00 +0800

    3.1 CapJackQ create project

---
+++ TakingYouFurther/TakingYouFurther/Classes/ViewControllers/BaseViewControllers/WLLDestinationViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLLDestinationViewController.h
+//  TakingYouFurther
+//
+//  Created by lanou3g on 16/2/29.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLLDestinationViewController : UIViewController
+
+@end
---
+++ TakingYouFurther/TakingYouFurther/Classes/ViewControllers/BaseViewControllers/WLLUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLLUserViewController.h
+//  TakingYouFurther
+//
+//  Created by lanou3g on 16/2/29.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLLUserViewController : UIViewController
+
+@end
